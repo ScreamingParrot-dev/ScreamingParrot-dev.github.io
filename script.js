3405e941ac5e15694401ca753b90e607d832ae4d
Leon Kazakov <71792170+ScreamingParrot-dev@users.noreply.github.com>
2025-06-17 05:00:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function resizeBackground() {
-  var extraHeight = 60; // Добавляем немного свободного пространства для компенсации изменяемости адресной строки
-  document.getElementById('bg1').style.height = window.innerHeight + extraHeight + "px";
-  document.getElementById('bg2').style.height = window.innerHeight + extraHeight + "px";
-}
-
-window.addEventListener('resize', resizeBackground); // Отслеживаем изменения размеров окна
-resizeBackground();
-
 document.addEventListener('DOMContentLoaded', () => {
   const elements = document.querySelectorAll('.program-container, .intro, .dress, .tips-text, .tips-block, .place-container, .important');
 
@@ -43,36 +34,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
   containers.forEach(container => {
     observer.observe(container);
-  });
-});
-
-document.getElementById('invitation-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-    
-  const formData = {
-    name: document.getElementById('name').value,
-    attendance: document.getElementById('attendance').value
-  };
-    
-
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbz51hWhnGPr7YnlP9hS2RhcHebfM1RVBUkN7Sn1YurvyXHmZBxQykGqPAC9ZfbNcC6KKw/exec';
-    
-  fetch(scriptUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-        body: JSON.stringify(formData)
-  })
-  .then(response => response.json())
-  .then(data => {
-    if(data.success) {
-      alert('Спасибо! Ваш ответ сохранен.');
-    } else {
-      alert('Ошибка: ' + data.message);
-    }
-  })
-  .catch(error => {
-    alert('Произошла ошибка при отправке: ' + error);
   });
 });
